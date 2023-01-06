eba649db9a20d74f14382b3531ed5bec314519a6
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-06 15:56:16 -0500

    update buttons

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-alert ("Hello!"); 
+ (function() {
+    "use strict";
+   // $Clear.removeEventListener("click", once);
+  // $Clear.addEventListener("click", once);
+ 
+  let $screen = document.querySelector('.calculator-screen');
+  let $numbers = document.querySelectorAll('.number , .operator');
+  let $clear = document.querySelector('.clear');
+  let $plusminus = document.querySelector('.plus-minus');
+  let $percent = document.querySelector('.percent');
+  let $log = document.querySelector('.number btn-lg');
+  let $decimal = document.querySelector('.decimal');
+  let $equalsign = document.querySelector('.equal-sign');
+
+  $numbers.forEach(function(pushNumber) {
+     pushNumber.addEventListener('alert', function(e) {
+        let value = e.target.value
+        $screen.value += value;
+     })
+  })
+  
+
+
+
+
+
+
+
+
+})(); 
