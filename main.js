a0b3008eee244e644488218be5f8e640c044fbc4
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-07 15:39:05 -0500

    add functions/event listeners

--- main.js
+++ main.js
@@ -3,22 +3,66 @@
    // $Clear.removeEventListener("click", once);
   // $Clear.addEventListener("click", once);
  
-  let $screen = document.querySelector('.calculator-screen');
-  let $numbers = document.querySelectorAll('.number , .operator');
-  let $clear = document.querySelector('.clear');
-  let $plusminus = document.querySelector('.plus-minus');
-  let $percent = document.querySelector('.percent');
-  let $log = document.querySelector('.number btn-lg');
-  let $decimal = document.querySelector('.decimal');
-  let $equalsign = document.querySelector('.equal-sign');
+ // const $screen = document.querySelector('.calculator-screen');
 
-  $numbers.forEach(function(pushNumber) {
-     pushNumber.addEventListener('alert', function(e) {
-        let value = e.target.value
-        $screen.value += value;
-     })
-  })
+ // select the buttons of the html page
+  const $numbers = document.querySelectorAll('.number');
+  const $operators = document.querySelectorAll('.operator');
+  const $clear = document.querySelector('.clear');
+  const $plusminus = document.querySelector('.plus-minus');
+  const $percent = document.querySelector('.percent');
+  const $log = document.querySelector('.number btn-lg');
+  const $decimal = document.querySelector('.decimal');
+  const $equalsign = document.querySelector('.equal-sign');
+
+//   Define a function named `pushNumber` that `alert()`s the number associated with its event argument 
+//  when called; add this function as an event listener for the number buttons
+
+  function pushNumber(event) {
+    console.log(event.target.value)
+   
+  }
+
+  function pushOperator(event) {
+    console.log(event.target.value)
+   
+  }
+
+  function calculate(event) {
+ console.log(event.target.value)
+
+  }
+
+
+$numbers.forEach(function(element){
+    element.addEventListener('click', pushNumber)
+})
+
+$operators.forEach(function(element){
+    element.addEventListener('click', pushOperator)
+})
+
+$equalsign.addEventListener('click', calculate)
+
+   
+
+
+
+
   
+
+  //displayValue string value that represents the input 
+  //firstOperand will store the first operand for any expression
+  //operator key will store the operator for an expression  
+  //waitingForSecondOperand heck if both the first operand and the operator have been input
+
+  //function updateDisplay() {
+  //  const $screen = document.querySelector('.calculator-screen');
+  //  $screen.value = calculator.displayValue;
+  
+  
+  //updateDisplay();
+
 
 
 
